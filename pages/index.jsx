b034060fa8530cbe7a537a03d143dda8a3b1307c
Juan Barroso <jbarroso@mymoid.com>
2022-08-16 21:06:41 +0100

    install and add basic box and ligth

---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import { Canvas } from "@react-three/fiber";
+import Floor from "../components/Floor";
+import css from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={css.scene}>
+      <Canvas
+        shadows={true}
+        className={css.canvas}
+        camera={{
+          position: [-6, 7, 7],
+        }}
+      >
+        <ambientLight color={"white"} intensity={0.3} />
+        <Floor />
+      </Canvas>
+    </div>
+  );
+}
